c611fb4ef5fc384687464704f9a0b700eeb1cab7
hammPa <121952935+hammPa@users.noreply.github.com>
2025-01-13 07:02:00 +0700

    Update 7-ReversedInteger.cpp

--- 7-ReversedInteger.cpp
+++ 7-ReversedInteger.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int reverse(int x) {
         int reversed = 0;
-        while(x != 0){
+        if(x == INT_MIN) return 0;
+    
+        bool isNegative = x < 0;
+        if (isNegative) x = -x;
+
+        while (x != 0) {
             int lastNum = x % 10;
+
+            if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && lastNum > INT_MAX % 10)) {
+                return 0;  // Overflow
+            }
+            
             reversed = reversed * 10 + lastNum;
             x /= 10;
         }
-        return reversed;
+        return isNegative ? -reversed : reversed;
     }
 };
